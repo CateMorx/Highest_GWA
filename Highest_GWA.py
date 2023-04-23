2dc30b1c0831733a525efc370b6454bdb3a6bf3c
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-04-23 14:25:04 +0800

    Pseudocode and text file

---
+++ Highest_GWA.py
@@ -0,0 +1,16 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 3 
+#Write a Python program that read a file containing the name of 20 
+#students together with their GWA. 
+#The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+
+#Imports necessary elements
+#Creates reference for printing
+#Creates Method For Printing student with highest gwa
+#opens students_gwa.txt
+#checks if the gwa is higher than the current highest gwa and replaces the values within the variables
+
+
+#calls the method
+#Prints and animates the final output
